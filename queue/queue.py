7d3f56e9beccf32522c8290490e01444422ddc56
Michael Bidnyk <mbidnyk1@gmail.com>
2020-06-08 18:17:26 -0500

    tests passing for queue and stack implementation

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,83 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+# 1. Queue class using an array as the underlying storage structure
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.size += 1
+#         return self.storage.insert(0,value)
+
+#     def dequeue(self):
+#         if not self.storage:
+#             return None
+#         else:
+#             self.size -= 1
+#             return self.storage.pop()
+            
+
+# q = Queue()
+# q.dequeue()
+# print(q.__len__())
+# print(q.size)
+
+# 2. Queue class, this time using the linked list implementation
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next_node = next
+    
+    def get_value(self):
+        return self.value
+    
+    def get_next(self):
+        return self.next_node
+    
+    def set_next(self, new_next):
+        self.next_node = new_next
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = None
+        self.head = None
+        self.tail = None
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        new_node = Node(value)
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
 
     def dequeue(self):
-        pass
+        if self.head is None:
+            return None
+        value = self.head.get_value()
+        if self.head is self.tail:
+            self.size -= 1
+            self.head = None
+            self.tail = None
+        else:
+            self.size -=1 
+            self.head = self.head.get_next()
+        return value
+            
+# 3. The data is managed in a slightly different way when implementing queues with linked list. 
+# Using an array/list the data set is structured and stored in one variable that you can then call methods on to enqueue and dequeue the values.
+# With linked lists the data set is grouped by nodes and an individual node needs to be manipulated to add and remove values from the queue.
+# This means that there is a lot more code needed to set up the nodes and test different cases for the linked list.
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,76 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+# 1. Implement the Stack class using an array as the underlying storage structure
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         return self.storage.append(value)
+
+#     def pop(self):
+#         if not self.storage:
+#             return None
+#         else:
+#             return self.storage.pop()
+
+# 2. Re-implement the Stack class, this time using the linked list implementation
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next_node = next
+    
+    def get_value(self):
+        return self.value
+    
+    def get_next(self):
+        return self.next_node
+    
+    def set_next(self, new_next):
+        self.next_node = new_next
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-
+        self.storage = None
+        self.head = None
+        self.tail = None
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.size += 1
+        new_node = Node(value)
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
 
     def pop(self):
-        pass
+        if self.tail is None:
+            return None
+        value = self.tail.get_value()
+        if self.head is self.tail:
+            self.size -= 1
+            self.head = None
+            self.tail = None
+        else:
+            self.size -=1 
+            current = self.head
+            while current.get_next() != self.tail:
+                current = current.get_next()
+            self.tail = current
+        return value
+
+# 3. The data is managed in a slightly different way when implementing queues with linked list. 
+# Using an array/list the data set is structured and stored in one variable that you can then call methods on to enqueue and dequeue the values.
+# With linked lists the data set is grouped by nodes and an individual node needs to be manipulated to add and remove values from the queue.
+# This means that there is a lot more code needed to set up the nodes and test different cases for the linked list.
